11cf0ed1c0d8ee43c1d171d5e675c2f55455cacb
NguyenBich11 <ntngocbich11122004@gmail.com>
2025-02-24 14:38:33 +0700

    add part practice of therory Week05 Theory

---
+++ LyThuyet/Tuan05/demoConnect/index.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        $host = "localhost";
+        $user = "admin";
+        $pass = "123456";
+        $db = "quanlytintuc";
+        $conn = mysqli_connect($host, $user, $pass, $db);
+
+        if($conn) {
+            insertLoaiTin($conn);
+            selectLoaiTin($conn);
+            updateLoaiTin($conn);
+            selectLoaiTin($conn);
+            deleteLoaiTin($conn);
+            selectLoaiTin($conn);
+        }else {
+            echo "<h1>Kết nối không thành công</h1>";
+        }
+        $conn->close();
+
+        function selectLoaiTin($conn) {
+            $selectSQL = "select IDLoaiTin, TenLoaiTin from loaitin";
+            $resutl = $conn->query($selectSQL);
+
+            if($resutl->num_rows > 0) {
+                while($rows = $resutl->fetch_assoc()){
+                    echo "<br>".$rows['TenLoaiTin'].$rows['IDLoaiTin']."<br>";
+                }
+            }else {
+                echo "Không có dữ liệu";
+            }
+        }
+
+        function insertLoaiTin($conn) {
+            $insertSQL = "insert into loaitin(TenLoaiTin, TheLoai) values('Nghệ thuật', 5)";
+            $resutl = $conn->query($insertSQL);
+            if($resutl) {
+                echo "Thêm dữ liệu vào bảng thành công";
+            }else {
+                echo "Thêm dữ liệu không thành công";
+            }
+        }
+
+        function updateLoaiTin($conn) {
+            $updateSQL = "update loaitin set TenLoaiTin = 'Báo chí' where TenLoaiTin = 'thể thao'";
+            $resutl = $conn->query($updateSQL);
+            if($resutl) {
+                echo "Update thành công từ Tên loại tin: thể thao -> Báo chí";
+            }else {
+                echo "Không thể update";
+            }
+        }
+
+        function deleteLoaiTin($conn) {
+            $updateSQL = "delete from loaitin where TenLoaiTin = 'Nghệ thuật'";
+            $resutl = $conn->query($updateSQL);
+            if($resutl) {
+                echo "Đã xóa thành công";
+            }else {
+                echo "Xóa không thành công";
+            }
+        }
+    ?>
+</body>
+</html>
